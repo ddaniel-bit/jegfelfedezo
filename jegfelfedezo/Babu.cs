5be2662291cb236c25c76270fdd9e50e7cae92b2
danko.daniel.janos <danko.daniel.janos@57-15>
2024-05-15 13:06:58 +0200

    Add project files.

---
+++ jegfelfedezo/Babu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jegfelfedezo
+{
+    internal class Babu
+    {
+        int azonosito;
+        int X;
+        int Y;
+        List<String> blacklist;
+        List<String> whitelist;
+
+        public Babu(int azonosito, int x, int y, List<string> blacklist, List<string> whitelist)
+        {
+            this.azonosito = azonosito;
+            X = x;
+            Y = y;
+            this.blacklist = blacklist;
+            this.whitelist = whitelist;
+        }
+
+        public int Azonosito { get => azonosito; set => azonosito = value; }
+        public int X1 { get => X; set => X = value; }
+        public int Y1 { get => Y; set => Y = value; }
+        public List<string> Blacklist { get => blacklist; set => blacklist = value; }
+        public List<string> Whitelist { get => whitelist; set => whitelist = value; }
+    }
+}
---
+++ jegfelfedezo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jegfelfedezo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            creategrid();
+        }
+
+        int magassag = 20;
+        int szelesseg = 20;
+        private void creategrid()
+        {
+            
+            for (int magass = 0; magass < magassag; magass++)
+            {
+                grdJeg.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int szeles = 0; szeles < szelesseg; szeles++)
+            {
+                grdJeg.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            grdJeg.ShowGridLines = true;
+            //gridetFeltolt();
+        }
+        private void babugeneralas()
+        {
+            int babuindex = 0;
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int y = rand.Next(1, magassag+1);
+                int x = rand.Next(1, szelesseg + 1);
+            } 
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
